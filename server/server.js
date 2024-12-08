0173c41a29b7f790d7bf1e99af65ad135edf51e6
Nidhi kurmi <115697832+Nidhi-877@users.noreply.github.com>
2024-12-08 20:09:14 +0530

    added files

--- server/server.js
+++ server/server.js
@@ -2,24 +2,26 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import EnvVar from "./configs/envVars.js";
-
 import DbConn from "./db/db.js";
 import cookieParser from "cookie-parser";
 import allroutes from "./routes/index.js";
+
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(morgan("combined"));
 app.use(cookieParser());
-const port = EnvVar.port;
+
+// Database Connection
 DbConn();
 
-app.get('/',(req,res)=>{
-  return res.status(200).send({message:"server is running in dev mode"})
-})
+// Routes
+app.get("/", (req, res) => {
+  return res.status(200).send({ message: "server is running in dev mode" });
+});
 app.use("/api/v1", allroutes);
 
-app.listen(port, () => {
-  console.log("server running on ports", port);
-});
+// Export the app for serverless use
+export default app;
